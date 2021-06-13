3fe2303d847afcdb4676b40e9871e90bd66571ca
Ronny Morel <rmorel@tredium.com>
2021-06-13 03:18:40 +0000

    first commit

---
+++ ch4/github/models.go
@@ -0,0 +1,27 @@
+package github
+
+import (
+	"time"
+)
+
+const IssueUrl = "https://api.github.com/search/issues"
+
+type IssueSearchResult struct {
+	TotalCount int `json:"total_count"`
+	Items      []*Issue
+}
+
+type Issue struct {
+	Number    int
+	HTMLURl   string `json:"html_url"`
+	Title     string
+	State     string
+	User      *User
+	CreatedAt time.Time `json:"created_at"`
+	Body      string
+}
+
+type User struct {
+	Login   string
+	HTMLURL string `json:"html_url"`
+}
